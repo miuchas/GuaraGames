98cf2027aeb0f0a1a05c598601867121e9b0c9fe
phelipe <phelipe@task.com.br>
2016-04-18 10:17:13 -0300

    configurações iniciais do projeto

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+      $this->publishes([
+      'vendor/bootstrap/dist' => public_path('vendor/bootstrap'),
+      ], 'public');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
