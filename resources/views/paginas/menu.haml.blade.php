43345b52bfb2d9558013f7e28e7d950c55370392
phelipe <phelipe@task.com.br>
2016-04-19 17:08:15 -0300

    preparando ambiente de login

--- resources/views/paginas/menu.haml.blade.php
+++ resources/views/paginas/menu.haml.blade.php
@@ -5,9 +5,17 @@
     Jogos
   %p.navbar-text
     Contato
-  %p.navbar-text.navbar-right.login
-    @if (Auth::guest())
-    Acesso
-    @else
-    {{ Auth::user()->name }}
+  %p.navbar-text.navbar-right.login#login_logout.dropdown-toggle.dropdown{"aria-expanded" => "true", "aria-haspopup" => "true", "data-toggle" => "dropdown", :type => "button"}
+    @if (Auth::guest()) Acesso
+    @else {{ Auth::user()->name }}
     @endif
+    %span.caret
+    %ul.dropdown-menu{"aria-labelledby" => "login_logout"}
+      %li
+        @if (Auth::guest())
+        %a{:href => "#"}
+          Login
+        @else
+        %a{:href => "#"}
+          Sair
+        @endif
