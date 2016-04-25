3e1fb393e8bc770a0fc7849f89755de962b37901
phelipe <phelipe@task.com.br>
2016-04-25 16:39:42 -0300

    colocando envio de emails pra funcionar

---
+++ resources/views/emails/confirmacao_cadastro.haml.blade.php
@@ -0,0 +1,15 @@
+%html
+  %head
+    %title Guara Games Studio
+
+    %meta{:charset => "utf-8"}
+    %meta{:name => "description", :content => "Guara Games Studio"}
+    %meta{:name => "keywords", :content => "Guara Games Studio"}
+    %meta{name: "viewport", content: "width=device-width, initial-scale=1.0"}
+    %meta{"http-equiv" => "X-UA-Compatible", "content" => "IE=edge"}
+
+    %body
+      %span
+        =$nome
+      %span
+        =$login
