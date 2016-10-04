f506dcc560fde144d0a9f894fc4b6a59ec1eb3d2
miuchas <phelipeevangelistasimimdiniz@gmail.com>
2016-10-03 23:33:22 -0300

    alterando organização do partial equipe

--- resources/views/paginas/equipe.haml.blade.php
+++ resources/views/paginas/equipe.haml.blade.php
@@ -2,17 +2,15 @@
   .fundo
     .titulo
       Equipe
-    .altura-geral.col-xs-12.col-sm-4
+    .altura
       .ph
         %img{ :src => "images/equipe/Ph.png" }
         %span.nome Phelipe Diniz
         %span.cargo Diretor Tecnico
-    .altura-geral.col-xs-12.col-sm-4
       .wads
         %img{ :src => "images/equipe/Wad.png" }
         %span.nome Wadson Freitas
         %span.cargo Diretor Geral
-    .altura-geral.col-xs-12.col-sm-4
       .lg
         %img{ :src => "images/equipe/Lg.png" }
         %span.nome Lucas Giraolla
